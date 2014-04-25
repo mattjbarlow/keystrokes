48e0f9906f02755df0f60dee37a65be9e4c15e35
Matt Barlow <matt.barlow@rackspace.com>
2014-04-25 06:26:10 -0500

    events

--- keycounts.py
+++ keycounts.py
@@ -1,8 +1,8 @@
 from evdev import InputDevice, categorize, ecodes
-import pdb
+import secrets
 from statsd import StatsClient
 
-c = StatsClient(host=GRAPHITE_URL, port=8126, prefix=None)
+c = StatsClient(host=secrets.GRAPHITE_URL, port=8126, prefix=None)
 
 dev = InputDevice('/dev/input/event0')
 
@@ -10,5 +10,3 @@ for event in dev.read_loop():
   if event.type == ecodes.EV_KEY:
     if event.value == 01:
       c.incr('keystroke', count=1, rate=1)
-
-
